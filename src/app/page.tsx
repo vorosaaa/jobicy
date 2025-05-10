206365baf0dac8a8b51985cbfa2daea753b855ff
Vörös Attila <vorosattila@icloud.com>
2025-05-10 18:24:14 +0200

    Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export const Home = () => {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -92,4 +92,4 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+};
